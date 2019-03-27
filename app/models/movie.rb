46670077511b5254705ce201027a2ad72433ce15
github username <github email address>
2019-03-27 10:48:13 -0700

    done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,7 +5,30 @@ class Movie
 
   def initialize(title)
     @title = title
-    self.class.all << self
+    @@all << self
+  end
+
+  def queue_items
+      QueueItem.all.select {|item| item.movie == self}
+  end
+
+  def viewers
+    self.queue_items.map {|item| item.viewer}
+  end
+
+  def average_rating
+    sum = self.queue_items.inject(0) {|sum, item| sum+item.rating.to_i}
+    average = sum.to_f/self.queue_items.length
+  end
+
+  def self.highest_rated
+    ratings = {}
+
+    self.all.each do |movie|
+      ratings["#{movie.title}"] = movie.average_rating
+    end
+    array = ratings.sort_by {|movie,rating| rating}
+    array[-1]
   end
 
   def self.all
